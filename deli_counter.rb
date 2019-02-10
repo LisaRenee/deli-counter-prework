c35b39fc5dfc6730ff5b5eda045148fd0abc7372
Lisa Scott <lrscot@aol.com>
2019-02-10 02:20:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,5 +20,10 @@ end
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
 
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is empty"
+  end
 end
