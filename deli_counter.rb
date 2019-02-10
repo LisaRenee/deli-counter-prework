3a09824526681003fda14132705dd06ed44bbe8a
Lisa Scott <lrscot@aol.com>
2019-02-10 01:50:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+#function currentLine(people){
+#  if(people.length === 0){
+#    return 'The line is currently empty.';
+#  }
+def line(people)
+  if people.length == 0
+    puts "The line is currently empty."
+  else
+    line_number = 1
+    phrase = "The line is currently: "
+    people.each do |person|
+      phrase += "#{line_number}. #{person} "
+      line_number += 1
+
+
+end
+    puts phrase
+  end
+end
